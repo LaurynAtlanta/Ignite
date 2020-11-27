4088da671b3da20376334761e6001e479b6de987
“Lauryn <laurynmills111@gmail.com>
2020-11-27 14:41:12 -0500

    styled home page

---
+++ src/pages/home.js
@@ -0,0 +1,55 @@
+import React,{useEffect} from 'react';
+//redux
+import {useDispatch, useSelector} from 'react-redux';
+import {loadGames} from '../actions/gamesAction';
+//components
+import Game from '../components/Game';
+//styling and animation
+import styled from 'styled-components';
+import {motion} from 'framer-motion';
+
+const Home = () => {
+    const dispatch = useDispatch();
+    useEffect(()=> {
+      dispatch(loadGames());
+    },[dispatch]); 
+    //extracting all items from redux state games
+    const {popular, newGames, upcoming} = useSelector((state) => state.games); 
+
+
+    return (  
+        <GameList>
+            <h2>Upcoming Games</h2>
+            <Games>
+                {upcoming.map((game)=>
+                     <Game 
+                        name={game.name} 
+                        released={game.released} 
+                        key={game.id} 
+                        image={game.background_image}/> 
+                )}
+            </Games>
+        </GameList>
+    );
+}
+ 
+
+const GameList = styled(motion.div) `
+    padding: 0rem 5rem;
+    h2{
+        padding: 5rem 0rem;
+    }
+`
+
+const Games = styled(motion.div)`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat( auto-fit , minmax(400px, 1fr)); 
+    // if it doesnt have  300px available, then jump down expand
+    grid-column-gap: 1rem;
+    grid-row-gap: 2rem;
+
+
+`
+
+export default Home;
